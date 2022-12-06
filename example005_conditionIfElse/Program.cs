82f3e99845c5ed22fb81abb5fd0cfdabee512bea
Dasha <dashabutenko@list.ru>
2022-12-06 17:04:17 +0400

    add example005_conditionIfElse project

---
+++ example005_conditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Please, enter your name: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "dasha")
+{
+    Console.WriteLine("Welcome back, Dasha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
