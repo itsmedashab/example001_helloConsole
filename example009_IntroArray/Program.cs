f8069fb85ddc791c32effcc96797c52e44cb3c3e
Dasha <dashabutenko@list.ru>
2022-12-07 22:33:00 +0400

    add example009_IntroArray

---
+++ example009_IntroArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1,42,35,42,5055,336,17,899};
+
+int n = array.Length;
+int find = 42;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
