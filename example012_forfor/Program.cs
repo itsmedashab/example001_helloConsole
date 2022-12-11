6b609b6854a900c1d41c7faf0aa4622ec01b01cb
Dasha <dashabutenko@list.ru>
2022-12-11 19:04:01 +0400

    add example012_forfor

---
+++ example012_forfor/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести на экран таблицу умножения
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.Write($"{i} * {j} = {i * j}\n");
+    }
+    Console.Write($"\n");
+}
