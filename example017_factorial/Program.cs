8a3af6a14ffd56b70eec3dbe68ddd42b10fb0b5f
Dasha <dashabutenko@list.ru>
2022-12-14 00:16:12 +0400

    example017_factorial

---
+++ example017_factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿// Факториал:
+
+double Factorial (int n)
+{
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.Write($"{i}! = {Factorial(i)}\n");
+}
+
+
+// Числа Фибоначчи:
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.Write($"{Fibonacci(i)}\n");
+}
